9f13d2113e4e73f9fb08368e39ade1df6f497443
ham-get <60130678+ham-get@users.noreply.github.com>
2021-07-03 16:25:54 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const formTest = document.forms;
+console.log(formTest);
+
+const formTestYourName = document.forms.form_test1.your_name;
+console.log(formTestYourName);
+
+const formTestYourNameValue = document.forms.form_test1.your_name.value;
+console.log(formTestYourNameValue);
+
+formTestYourName.addEventListener('input',() => {
+  let yourNameValue = document.getElementById('your_name_value');
+  yourNameValue.textContent = formTestYourName.value.length;
+})
+
+const preview = document.forms.form_test1.preview;
+document.forms.form_test1.image_file.addEventListener('change', e => {
+  
+  const uploadFile = e.target.files[0];
+
+  const reader = new FileReader();
+  reader.addEventListener('load', ()=> {
+    preview.src = reader.result;
+    console.log(preview.src);
+  });
+  reader.readAsDataURL(uploadFile);
+});
